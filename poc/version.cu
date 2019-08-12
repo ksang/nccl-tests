ad6ee3deb3426c2bcce075545d7eed5b1cdeb262
ksang <kaitoy@qq.com>
2019-08-12 16:00:52 +0800

    nccl version test

---
+++ poc/version.cu
@@ -0,0 +1,30 @@
+#include "cuda_runtime.h"
+#include "nccl.h"
+#include <stdio.h>
+
+#define CUDACHECK(cmd) do {                         \
+  cudaError_t e = cmd;                              \
+  if( e != cudaSuccess ) {                          \
+    printf("Failed: Cuda error %s:%d '%s'\n",       \
+        __FILE__,__LINE__,cudaGetErrorString(e));   \
+    exit(EXIT_FAILURE);                             \
+  }                                                 \
+} while(0)
+
+
+#define NCCLCHECK(cmd) do {                         \
+  ncclResult_t r = cmd;                             \
+  if (r!= ncclSuccess) {                            \
+    printf("Failed, NCCL error %s:%d '%s'\n",       \
+        __FILE__,__LINE__,ncclGetErrorString(r));   \
+    exit(EXIT_FAILURE);                             \
+  }                                                 \
+} while(0)
+
+int main(int argc, char* argv[])
+{
+    int version = 0;
+    NCCLCHECK(ncclGetVersion(&version));
+    printf("NCCL version: %d\n", version);
+    return 0;
+}
